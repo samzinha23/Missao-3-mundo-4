e5853c11a60189381869a24a61540d725b6e6e5c
samzinha23 <167657993+samzinha23@users.noreply.github.com>
2025-05-15 22:48:21 -0300

    Add files via upload

---
+++ Trabalho/Missao-3-Mundo-4/Microatividades/ListaDeTarefas/app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.listadetarefas"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.listadetarefas"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("com.google.android.gms:play-services-wearable:18.0.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
